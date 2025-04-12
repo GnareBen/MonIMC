949e9a1f2f465e1a9922dd4526342977d56588c3
Gnaré Ben Ibrahim <bengnareibrahim@gmail.com>
2025-04-12 04:05:49 +0000

    Refactor PrimaryButton to use FilledButton.icon and add optional icon parameter

--- lib/widgets/confirmation_button.dart
+++ lib/widgets/confirmation_button.dart
@@ -2,31 +2,25 @@ import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
   final String text;
+  final IconData? icon;
   final VoidCallback onPressed;
 
-  const PrimaryButton({super.key, required this.text, required this.onPressed});
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    this.icon,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.green,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+    return FilledButton.icon(
+      icon: icon != null ? Icon(icon, size: 20) : const SizedBox(),
+      label: Text(text),
+      onPressed: onPressed,
+      style: FilledButton.styleFrom(
+        minimumSize: const Size.fromHeight(50),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       ),
     );
   }
